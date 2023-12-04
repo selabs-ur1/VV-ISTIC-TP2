ccf2c10093b26c594a4de77e05c5dd9b7c7628b5
Rémi Rativel <remi.rativel@etudiant.univ-rennes1.fr>
2023-12-04 12:22:32 +0100

    no-getter exercise

--- code/Exercise4/src/main/java/fr/istic/vv/Main.java
+++ code/Exercise4/src/main/java/fr/istic/vv/Main.java
@@ -1,18 +1,9 @@
 package fr.istic.vv;
 
-import com.github.javaparser.Problem;
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.visitor.VoidVisitor;
-import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import com.github.javaparser.utils.SourceRoot;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public class Main {
 
     public static void main(String[] args) throws IOException {
@@ -28,7 +19,7 @@ public class Main {
         }
 
         SourceRoot root = new SourceRoot(file.toPath());
-        PublicElementsPrinter printer = new PublicElementsPrinter();
+        InaccessibleElementsPrinter printer = new InaccessibleElementsPrinter();
         root.parse("", (localPath, absolutePath, result) -> {
             result.ifSuccessful(unit -> unit.accept(printer, null));
             return SourceRoot.Callback.Result.DONT_SAVE;
